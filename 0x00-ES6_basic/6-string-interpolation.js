5fbc0e97861b8fd518cbf267f90bc3c1a9495516
Ezekiel Moses <codewithprince.com@gmail.com>
2023-06-28 01:56:49 +0100

    fixed buggy return statement

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,4 +1,4 @@
-function getSanFranciscoDescription() {
+export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
     income: '$119,868',
@@ -6,15 +6,5 @@ function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return `As of 
-   ${year}
-    , it was the seventh-highest income county in the United States 
-      , with a per capita personal income of 
-    budget.${budget.income}
-    . As of 2015, San Francisco   proper had a GDP of 
-    budget.${budget.gdp}+
-    , and a GDP per capita of 
-    budget. ${budget.capita}
-    .`;
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a gdp of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
-console.log(getSanFranciscoDescription());
