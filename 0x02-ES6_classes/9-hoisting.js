0e8c216de0ab7e9aa0064846df00bf01ce1613bb
Ezekiel Moses <codewithprince.com@gmail.com>
2023-06-30 01:06:31 +0100

    fixed esLint underscore

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,49 +1,40 @@
-/* eslint-disable no-underscore-dangle */
 /* eslint-disable max-classes-per-file */
 export class HolbertonClass {
   constructor(year, location) {
-    this._year = year;
-    this._location = location;
-  }
-
-  get year() {
-    return this._year;
-  }
-
-  get location() {
-    return this._location;
+    this.year = year;
+    this.location = location;
   }
 }
 
 export class StudentHolberton {
-  /* eslint-disable */
   constructor(firstName, lastName, holbertonClass) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.holbertonClass = holbertonClass;
   }
 
   get fullName() {
-    return `${this._firstName} ${this._lastName}`;
-  }
-
-  get holbertonClass() {
-    return this._holbertonClass;
+    return `${this.firstName} ${this.lastName}`;
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this.firstName} ${this.lastName} - ${this.holbertonClass.year} - ${this.holbertonClass.location}`;
   }
 }
 
-const class2019 = new HolbertonClass(2019, "San Francisco");
-const class2020 = new HolbertonClass(2020, "San Francisco");
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-const student1 = new StudentHolberton("Guillaume", "Salva", class2020);
-const student2 = new StudentHolberton("John", "Doe", class2020);
-const student3 = new StudentHolberton("Albert", "Clinton", class2019);
-const student4 = new StudentHolberton("Donald", "Bush", class2019);
-const student5 = new StudentHolberton("Jason", "Sandler", class2019);
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
-export default listOfStudents;
+export const listOfStudents = [
+  student1,
+  student2,
+  student3,
+  student4,
+  student5,
+];
